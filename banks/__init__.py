e2ac92f5f27cf151a2e7e57fe4920b2fbef6704b
Anatoli Babenia <anatoli@rainforce.org>
2017-05-06 17:28:24 +0300

    Stub for bank information - csv data and Python request/update

---
+++ banks/__init__.py
@@ -0,0 +1,8 @@
+"""
+Data for banks of Republic of Belarus
+"""
+
+import csv
+import os
+
+DATAFILE = os.path.join(os.path.dirname(__file__), 'banks.csv')
---
+++ banks/update.py
@@ -0,0 +1,7 @@
+"""
+Parse bank data from http://www.nbrb.by/engl/system/banks.asp
+"""
+
+class BankData:
+  number
+
